f02e69d3d621f057e92854754eb205ae0e7c4dd0
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-11-23 04:34:13 +0000

    feat: build docker image

--- portal/src/projects/index.ts
+++
@@ -1,9 +0,0 @@
-import { BilyObrazProject } from "./bily-obraz.project";
-import { KorzoNarodni2023 } from "./korzo-narodni-2023.project";
-
-export const Projects = [
-  // Pomezi
-  BilyObrazProject,
-  // Korzo Narodni
-  KorzoNarodni2023,
-];
--- portal/src/projects/schema/project.ts
+++
@@ -1,7 +0,0 @@
-export interface Project {
-  id: string;
-  name: string;
-  folder?: string;
-  url: string;
-  public?: boolean;
-}
